934ed53bc3ec0ef4f35febfb35d22d5f3e74f9ac
Jit Jana <jitjana755@gmail.com>
2026-09-07 23:32:23 +0530

    Add employee class practice

---
+++ 03 _int_self.py
@@ -0,0 +1,14 @@
+class employee():
+    language = "python and c"
+    salary = 10000
+    def getinfo(self):
+        print(f"the salary{self.salary}. and the language{self.language}")
+    def geet(self):
+        print("good morning")
+
+
+jit = employee()
+jit.name = "kobita jana"
+
+jit.getinfo()
+jit.geet()
